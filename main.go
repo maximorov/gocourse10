a1509dd2f1c205e3e2efef3b8bc31db3a85ace4f
maks <m.morozov@autodoc.eu>
2022-11-17 20:40:37 +0200

    webserver

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	rtr := mux.NewRouter()
+	srv := &http.Server{
+		Addr:              `0.0.0.0:8080`,
+		ReadTimeout:       time.Millisecond * 200,
+		WriteTimeout:      time.Millisecond * 200,
+		IdleTimeout:       time.Second * 10,
+		ReadHeaderTimeout: time.Millisecond * 200,
+		Handler:           rtr,
+	}
+
+	rtr.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		_, _ = rw.Write([]byte("Hello!"))
+	})
+	rtr.HandleFunc("/healthcheck", func(rw http.ResponseWriter, r *http.Request) {
+		_, _ = rw.Write([]byte(`OK`))
+	})
+
+	http.Handle("/", rtr)
+
+	go func() {
+		log.Println(`Web Server started`)
+		err := srv.ListenAndServe()
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal(err)
+		}
+	}()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+	)
+	<-done
+
+	log.Println(`Web Server is shutting down`)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(ctx, err)
+	}
+}
